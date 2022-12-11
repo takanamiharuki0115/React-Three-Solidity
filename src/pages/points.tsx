f7955d051b02cf0f51a1d58f55fdc66b842deb79
IhorKrs <ihorkrskyi@gmail.com>
2022-12-11 06:33:45 -0500

    Update points.tsx

--- src/pages/points.tsx
+++ src/pages/points.tsx
@@ -6,7 +6,7 @@ const CubePoints = dynamic(() => import('../components/canvas/CubePoints'), { ss
 const Page = (props: any) => {
   return (
     <Instructions>
-      This is the <span className='text-green-200'>/blob</span> route. Click on the blob to navigate back. The canvas was
+      This is the <span className='text-green-200'>/points</span> route. Click on the points to navigate to the next route. The canvas was
       not unmounted between route changes, only its contents. If you want scene contents to persist, put them into{' '}
       <span className='text-green-200'>@/components/canvas/Canvas</span>.
     </Instructions>
